a60400581f70dda2748fc82fc0cf6e6e8f05f5cc
Vrinda Shroff <vrindashroff.india@gmail.com>
2022-10-28 16:08:36 -0700

    adding loading/ mkdir functionality

--- main.py
+++ main.py
@@ -8,12 +8,14 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
-from flask import Flask, render_template, request\
-
-
+from flask import Flask, render_template, request
+import pandas as pd
+import json
+import requests
+import sys
+import math
 
 app = Flask(__name__)
-
 
 @app.route('/form')
 def form():
@@ -28,10 +30,38 @@ def data():
         form_data = request.form
         print(form_data.items())
         ##makedir(form_data)
+        for i, v in form_data.items():
+            print(i,v)
+        print(form_data.get('Name'), form_data.get('file'))
+        loadData(form_data.get('Name'),form_data.get('file'))
         return render_template('data.html', form_data=form_data)
 
 app.run(host='localhost', port=5000)
 
+def loadData(name, filename):
+    data_set = pd.read_csv(filename)
+    length = data_set.shape[0]
+    num_partitions = math.ceil(length/100)
+    partiton_path =  ""
+    for i in range(1,num_partitions+1):
+        if i == num_partitions:
+            x = data_set.iloc[((i-1)*100+1):len(data_set)+1, :].to_json(orient="index")
+        else: 
+            x = data_set.iloc[((i-1)*100+1):(i*100)+1, :].to_json(orient="index")
+        db = json.loads(x)
+        q = json.dumps(db)
 
-##def makedir(form_data):
+        f = open('sample.json','w')
+        f.write(q)
+        f.close()
+        name1 = "data"+str(i)
+        p = "p"+str(i)
+        url = 'https://project551-a12dc-default-rtdb.firebaseio.com/datanode/{}/.json'.format(name1)
+        partiton_path += '"{}":"https://project551-a12dc-default-rtdb.firebaseio.com/datanode/{}/.json",'.format(p,name1)
+        r1 = requests.put(url, data = q) 
+    path1 = partiton_path[:-1]
+    print(path1)
+    path1 = "{"+path1+"}"
+    r2 = requests.put('https://project551-a12dc-default-rtdb.firebaseio.com/namenode/{}/.json'.format(name1), data = json.dumps(path1))
+#def makedir(form_data):
     ##print(form_data.items())
