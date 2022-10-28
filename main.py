0bd35f374e64b36a3041780476821aa5eb21e8c1
sanjithverma <sanjitha@usc.edu>
2022-10-28 11:31:30 -0700

    first

---
+++ main.py
@@ -0,0 +1,37 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
+
+# Press the green button in the gutter to run the script
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+from flask import Flask, render_template, request\
+from werkzeug import secure_filename
+
+
+app = Flask(__name__)
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/data/', methods=['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        print(form_data.items())
+        makedir(form_data)
+        return render_template('data.html', form_data=form_data)
+
+app.run(host='localhost', port=5000)
+
+
+def makedir(form_data):
+    print(form_data.items())
