4f5f459123391f4346bf3af923151b44fe583528
sanjithverma <sanjitha@usc.edu>
2022-10-30 15:33:08 -0700

    DDL

--- mainSQL.py
+++ mainSQL.py
@@ -5,10 +5,14 @@ cnx = mysql.connector.connect(user='root', password='Harshsonita@123',
                               database='new_schema', auth_plugin='mysql_native_password')
 
 mycursor = cnx.cursor()
-##mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+mycursor.execute("CREATE TABLE NAMENODE (parent VARCHAR(255), child VARCHAR(255), type VARCHAR(255))")
+mycursor.execute("CREATE TABLE FILEATTRIBUTES(filename VARCHAR(255) PRIMARY KEY, partitionn VARCHAR(255), filesize VARCHAR(255))")
 
 mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
   print(x)
 cnx.close()
+
+
+
