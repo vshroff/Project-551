da5e96234079d8fb48b22d923a11c1c180706e1c
Advaith <ms.advaith98@gmail.com>
2022-11-27 01:24:43 -0800

    added sql functios

--- mainSQL.py
+++ mainSQL.py
@@ -1,4 +1,8 @@
 import mysql.connector
+import math
+import pandas as pd
+import json
+import requests
 
 cnx = mysql.connector.connect(user='root', password='Harshsonita@123',
                               host='localhost',
@@ -57,6 +61,114 @@ def makedir(dirname):
         q = "INSERT INTO NAMENODE VALUES({},{})".format(names[-2], names[-1])
         mycursor.execute(q)
         cnx.commit()
+
+
+
+def ls(dirname):
+        if dirname == '/':
+            q3 = "SELECT CHILD FROM NAMENODE WHERE PARENT = '{}'".format(dirname)
+            mycursor.execute(q3)
+            child = mycursor.fetchall()
+            return child   
+        names = dirname.split('/')
+        name = names[-1]
+        
+        q3 = "SELECT CHILD FROM NAMENODE WHERE PARENT = '{}'".format(name)
+        mycursor.execute(q3)
+        child = mycursor.fetchall()
+        print(child)
+
+
+def loadData(name, filename, num_partitions):
+    data_set = pd.read_csv(filename)
+    length = data_set.shape[0]
+    num_rows_per_part = math.ceil(length/num_partitions)
+    partiton_path = ""
+    for i in range(1, num_partitions + 1):
+        if i == num_partitions:
+            x = data_set.iloc[((i - 1) * num_rows_per_part + 1):len(data_set) + 1, :].to_json(orient="index")
+        else:
+            x = data_set.iloc[((i - 1) * num_rows_per_part + 1):(i * num_rows_per_part) + 1, :].to_json(orient="index")
+        
+        name1 = filename.split('.')[0] + str(i)
+        p = "p" + str(i)
+        tabname = name1 + p
+        qCreate = "CREATE TABLE {}".format(tabname)
+        mycursor.execute(qCreate)
+        cnx.commit()
+
+
+        cols = "`,`".join([str(i) for i in x.columns.tolist()])
+
+        for i, row in x.iterrows():
+            sql = "INSERT INTO `{}` (`"+cols+"`) VALUES (" + "%s,"*len(row-1)+"%s)".format(tabname)
+            mycursor.execute(sql, tuple(row))
+        
+
+        
+        ##url = 'https://project551-a12dc-default-rtdb.firebaseio.com/datanode/{}/{}/.json'.format(name, name1)
+        ##partiton_path += '"{}":"https://project551-a12dc-default-rtdb.firebaseio.com/datanode/{}/{}/.json",'.format(p,name,name1)
+       ## r1 = requests.put(url, data=q)
+    path1 = partiton_path[:-1]
+    path1 = "{" + path1 + "}"
+    db = json.loads(path1)
+    q = json.dumps(db)
+    query = "INSERT INTO FILEATTRIBUTES VALUES (`filename`, `num_partitions`,`length`)"
+
+   ## r2 = requests.put('https://project551-a12dc-default-rtdb.firebaseio.com/namenode/{}/{}/.json'.format(name,filename.split('.')[0]),data=q)
+
+
+
+
+def getPartitions(filename):
+    query = "SELECT Partitionn from FILEATTRIBUTES where filename = {}".format(filename)
+    mycursor.execute(query)
+    parent = mycursor.fetchall()
+    return parent
+
+
+    
+
+def readPartitions(filename,partition):
+        tabname = filename + "p"+str(partition)
+        sql = "SELECT * FROM  {}".format(tabname)
+        mycursor.execute(sql)
+        res = mycursor.fetchall()
+        print(res)
+
+def rm(filename):
+    n = getPartitions(filename)
+    for i in range(n):
+        tabname = filename + "p"+str(i)
+        query = "DELETE FROM {}".format(tabname)
+        mycursor.execute(query)
+        cnx.commit()
+
+    q = "DELETE FROM FILEATTRIBUTES WHERE FILENAME = {}".format(filename)
+    mycursor.execute(q)
+    cnx.commit()
+
+def cat(filename):
+    n = getPartitions(filename)
+    for i in range(1, n+1):
+        tabname = filename + "p"+str(i)
+        sql = "SELECT * FROM  {}".format(tabname)
+        mycursor.execute(sql)
+        res = mycursor.fetchall()
+        print(res)
+
+            
+
+
+        
+
+
+
+
+
+
+
+        
 
 
 
